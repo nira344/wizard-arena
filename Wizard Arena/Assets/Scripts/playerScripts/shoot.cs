f13120d3d816ae9806de0e9de9c776cd45334e3e
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-04-08 13:22:41 -0500

    Changed Controls

--- Wizard Arena/Assets/Scripts/playerScripts/shoot.cs
+++ Wizard Arena/Assets/Scripts/playerScripts/shoot.cs
@@ -13,7 +13,7 @@ public class shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire2"))
         {
             HealthAndMana statScript = GetComponent<HealthAndMana>();
             if (statScript != null)
