f077af093408df55a0fb207577aba9c0978d24c0
AutumnLeaf2 <lii973620@gmail.com>
2025-04-08 13:43:10 -0500

    FIREBALL

--- Wizard Arena/Assets/Scripts/playerScripts/shoot.cs
+++ Wizard Arena/Assets/Scripts/playerScripts/shoot.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class shoot : MonoBehaviour
 {
     public GameObject fireballPrefab;
+    public GameObject iceshardPrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,15 +19,31 @@ public class shoot : MonoBehaviour
             HealthAndMana statScript = GetComponent<HealthAndMana>();
             if (statScript != null)
             {
-                if (statScript.currentMana > 0)
+                if (statScript.currentMana >= 1)
                 {
                     statScript.currentMana--;
-                    var fireball = Instantiate(fireballPrefab, transform.position, transform.rotation);  // Instantiate the fireball
+                    var iceShard = Instantiate(iceshardPrefab, transform.position, transform.rotation);  // Instantiate the iceshard
                 }
             }
             else
             {
-                var fireball = Instantiate(fireballPrefab, transform.position, transform.rotation);  // Instantiate the fireball
+                var iceShard = Instantiate(iceshardPrefab, transform.position, transform.rotation);  // Instantiate the iceshard
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            HealthAndMana statScript = GetComponent<HealthAndMana>();
+            if (statScript != null)
+            {
+                if (statScript.currentMana >= 3)
+                {
+                    statScript.currentMana-= 3;
+                    var fierBall = Instantiate(fireballPrefab, transform.position, transform.rotation);  // Instantiate the fireball
+                }
+            }
+            else
+            {
+                var fireBall = Instantiate(fireballPrefab, transform.position, transform.rotation);  // Instantiate the fireball
             }
         }
     }
