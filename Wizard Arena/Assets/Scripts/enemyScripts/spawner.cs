9e680d4e6496cc021a1dfc4ad79c1429b82194e3
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-04-09 13:33:19 -0500

    Spawner v0.5

--- Wizard Arena/Assets/Scripts/enemyScripts/spawner.cs
+++ Wizard Arena/Assets/Scripts/enemyScripts/spawner.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class spawner : MonoBehaviour
 {
     public GameObject[] prefabs;
     public float spawnCooldown;
-    public int maxEnemiesSpawned;
+    public int maxEnemies;
 
-    private int currentEnemiesSpawned;
+    public List<GameObject> spawnedEnemyObjects;
+    public float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnedEnemyObjects = new List<GameObject>();
+        spawnedEnemyObjects.Capacity = maxEnemies;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            if (spawnedEnemyObjects.Count < maxEnemies)
+            {
+                GameObject goober = prefabs[0];
+                Instantiate(goober, gameObject.transform.position, Quaternion.identity);
+                timer = spawnCooldown;
+                spawnedEnemyObjects.Insert(0, goober);
+            }
+        }
     }
 }
