84ba6932999e1a777cafc0ad0e6dd5987e27962f
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-04-09 13:12:42 -0500

    Spawner Object Created

---
+++ Wizard Arena/Assets/Scripts/enemyScripts/spawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public float spawnCooldown;
+    public int maxEnemiesSpawned;
+
+    private int currentEnemiesSpawned;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
