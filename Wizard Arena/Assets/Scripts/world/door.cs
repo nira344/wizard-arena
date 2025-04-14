0d3436e7080ee95fdadaf2f9eaee7c45940f80da
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-04-14 13:53:26 -0500

    Doorways now lead to T H E  V O I D

--- Wizard Arena/Assets/Scripts/world/door.cs
+++ Wizard Arena/Assets/Scripts/world/door.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class door : MonoBehaviour
 {
+    public string sceneToLoad;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,12 @@ public class door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (sceneToLoad != null)
+        {
+            if (Input.GetKeyDown("f"))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
     }
 }
