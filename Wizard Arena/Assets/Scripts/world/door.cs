5de191448dbc2a7f0fdd8afbdad9d452eb502814
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-04-16 14:48:26 -0500

    Doors Fixed

--- Wizard Arena/Assets/Scripts/world/door.cs
+++ Wizard Arena/Assets/Scripts/world/door.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class door : MonoBehaviour
 {
     public string sceneToLoad;
+    private GameObject player;
+    private bool activated = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -16,10 +18,15 @@ public class door : MonoBehaviour
     {
         if (sceneToLoad != null)
         {
-            if (Input.GetKeyDown("f"))
+            if (Input.GetKeyDown("f") && TouchingPlayer() && activated)
             {
                 SceneManager.LoadScene(sceneToLoad);
             }
         }
+    }
+
+    private bool TouchingPlayer()
+    {
+        return gameObject.GetComponent<BoxCollider2D>().IsTouching(player.GetComponent<BoxCollider2D>());
     }
 }
