068896b5442f0839627f251d786db7faee8c45d2
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-04-21 13:31:32 -0500

    Created Basic First House

--- Wizard Arena/Assets/Scripts/world/door.cs
+++ Wizard Arena/Assets/Scripts/world/door.cs
@@ -4,21 +4,24 @@ using UnityEngine.SceneManagement;
 public class door : MonoBehaviour
 {
     public string sceneToLoad;
+    public bool locked = false;
+    public float spawnX;
+    public float spawnY;
+
     private GameObject player;
-    private bool activated = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         if (sceneToLoad != null)
         {
-            if (Input.GetKeyDown("f") && TouchingPlayer() && activated)
+            if (Input.GetKeyDown("f") && TouchingPlayer() && !locked)
             {
                 SceneManager.LoadScene(sceneToLoad);
             }
@@ -28,5 +31,15 @@ public class door : MonoBehaviour
     private bool TouchingPlayer()
     {
         return gameObject.GetComponent<BoxCollider2D>().IsTouching(player.GetComponent<BoxCollider2D>());
+    }
+
+    public void Lock()
+    {
+        locked = true;
+    }
+
+    public void Unlock()
+    {
+        locked = false;
     }
 }
