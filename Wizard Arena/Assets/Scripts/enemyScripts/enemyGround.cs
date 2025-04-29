2bf7b68deb137d38e0dbd5d7e0215de7a4eb9a1b
nira344 <bluey9091@gmail.com>
2025-04-29 14:20:34 -0500

    this one adds the idle sounds

--- Wizard Arena/Assets/Scripts/enemyScripts/enemyGround.cs
+++ Wizard Arena/Assets/Scripts/enemyScripts/enemyGround.cs
@@ -7,12 +7,30 @@ public class enemyGround : MonoBehaviour
     public float speed;
     GameObject player;
     Rigidbody2D rb;
+    
+    public AudioSource audiosource;
+    public AudioClip idle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         rb = gameObject.GetComponent<Rigidbody2D>();
+
+         if (audiosource == null)
+        {
+            audiosource = GetComponent<AudioSource>();
+        }
+
+        // Start the Coroutine to play the idle sound at random intervals
+        if (audiosource != null && idle != null)
+        {
+            StartCoroutine(PlayIdleSound());
+        }
+        else
+        {
+            Debug.LogWarning("AudioSource or IdleSoundClip is missing.");
+        }
     }
 
     // Update is called once per frame
@@ -53,6 +71,20 @@ public class enemyGround : MonoBehaviour
 
             // move in the direction we have calculated
             transform.Translate(direction);
+        }
+    }
+
+    // Coroutine to play the idle sound at random intervals
+    private System.Collections.IEnumerator PlayIdleSound()
+    {
+        while (true)
+        {
+            // Wait for a random time between 7 and 10 seconds
+            float waitTime = Random.Range(7f, 10f);
+            yield return new WaitForSeconds(waitTime);
+
+            // Play the idle sound
+            audiosource.PlayOneShot(idle);
         }
     }
 
