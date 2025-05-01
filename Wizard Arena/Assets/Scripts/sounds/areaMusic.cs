146723bef1a980904ee90aab2c5e2bc9cb5f4c56
Skeleton <126999295+CoolestDudeA17@users.noreply.github.com>
2025-05-01 12:42:35 -0500

    grass (yippee!!)

--- Wizard Arena/Assets/Scripts/sounds/areaMusic.cs
+++ Wizard Arena/Assets/Scripts/sounds/areaMusic.cs
@@ -4,15 +4,18 @@ public class areaMusic : MonoBehaviour
 {
     public AudioSource music;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.LogError("something just happened");
         if (collision.CompareTag("Player"))
         {
+            Debug.LogError("playing music");
             music.Play();
+            Debug.LogError("music started");
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
